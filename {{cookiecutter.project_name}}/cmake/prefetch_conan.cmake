960bca8832edec1a6ff892649af3240a2dd94551
Gavin Chang <gavin.clt@gmail.com>
2021-03-19 10:14:30 +0800

    Use "current" dir in prefetch script

--- {{cookiecutter.project_name}}/cmake/prefetch_conan.cmake
+++ {{cookiecutter.project_name}}/cmake/prefetch_conan.cmake
@@ -1,17 +1,17 @@
 # Excerpted from https://github.com/conan-io/cmake-conan
-list(APPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR})
-list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR})
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_BINARY_DIR})
+list(APPEND CMAKE_PREFIX_PATH ${CMAKE_CURRENT_BINARY_DIR})
 
-if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/conan.cmake")
 message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
 file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/v0.16.1/conan.cmake"
               "${CMAKE_BINARY_DIR}/conan.cmake"
               TLS_VERIFY ON)
 endif()
 
-include(${CMAKE_BINARY_DIR}/conan.cmake)
+include(${CMAKE_CURRENT_BINARY_DIR}/conan.cmake)
 
-configure_file(${CMAKE_SOURCE_DIR}/conanfile.txt ${CMAKE_BINARY_DIR}/conanfile.txt COPYONLY)
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/conanfile.txt ${CMAKE_CURRENT_BINARY_DIR}/conanfile.txt COPYONLY)
 
 conan_cmake_autodetect(settings)
 
