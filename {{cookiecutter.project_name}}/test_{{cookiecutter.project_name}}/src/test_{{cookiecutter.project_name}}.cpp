0a52537a5fcf0296b9ea0fe48983bebd1ac7e081
Gavin Chang <gavin.clt@gmail.com>
2022-01-04 15:24:45 +0800

    Add Catch2 support

--- {{cookiecutter.project_name}}/test_{{cookiecutter.project_name}}/src/test_{{cookiecutter.project_name}}.cpp
+++ {{cookiecutter.project_name}}/test_{{cookiecutter.project_name}}/src/test_{{cookiecutter.project_name}}.cpp
@@ -1,7 +1,15 @@
+{%- if cookiecutter.testing_framework == "gtest"%}
 #include <gtest/gtest.h>
+{%- else %}
+#include <catch2/catch.hpp>
+{%- endif %}
 {%- if cookiecutter.project_type != "exe" %}
 #include "{{cookiecutter.project_name}}.hpp"
 {%- endif %}
 
+{%- if cookiecutter.testing_framework == "gtest"%}
 TEST(Test_{{cookiecutter.project_name}}, {{cookiecutter.project_name}}) {
+{%- else %}
+    TEST_CASE("Test_{{cookiecutter.project_name}}", "{{cookiecutter.project_name}}") {
+{%- endif %}
 }
