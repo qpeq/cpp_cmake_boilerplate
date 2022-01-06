3fb59d9e46a43d2fe14593bd33c1f3dc44ab7cdc
Gavin Chang <gavin.clt@gmail.com>
2022-01-06 09:42:10 +0800

    Use cmake generator

--- {{cookiecutter.project_name}}/test_{{cookiecutter.project_name}}/src/test_{{cookiecutter.project_name}}.cpp
+++ {{cookiecutter.project_name}}/test_{{cookiecutter.project_name}}/src/test_{{cookiecutter.project_name}}.cpp
@@ -1,4 +1,4 @@
-{%- if cookiecutter.testing_framework == "gtest"%}
+{%- if cookiecutter.testing_framework == "gtest" -%}
 #include <gtest/gtest.h>
 {%- else %}
 #include <catch2/catch.hpp>
@@ -7,9 +7,9 @@
 #include "{{cookiecutter.project_name}}.hpp"
 {%- endif %}
 
-{%- if cookiecutter.testing_framework == "gtest"%}
+{% if cookiecutter.testing_framework == "gtest" -%}
 TEST(Test_{{cookiecutter.project_name}}, {{cookiecutter.project_name}}) {
 {%- else %}
-    TEST_CASE("Test_{{cookiecutter.project_name}}", "{{cookiecutter.project_name}}") {
-{%- endif %}
+TEST_CASE("Test_{{cookiecutter.project_name}}", "{{cookiecutter.project_name}}") {
+{% endif -%}
 }
--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/inc/{{cookiecutter.project_name}}.hpp
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/inc/{{cookiecutter.project_name}}.hpp
@@ -1,3 +1,3 @@
 #pragma once
 
-void hello();
+void {{cookiecutter.project_name}}();
